82f5a6741d686dc947e2e248271036bc831c3cf5
Joacim Boive <joacim.boive@gmail.com>
2017-10-26 17:40:36 +0200

    Fixed URL to not att empty ?

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,6 +1,12 @@
 export const testCanonical = (url, expectedCanonical, randString = '') => {
+  let separator = '';
+
+  if (randString !== '') {
+    separator = url.indexOf('?') > -1 ? '&' : '?';
+  }
+
   cy
-    .visit(url + (url.indexOf('?') > -1 ? '&' : '?') + randString)
+    .visit(url + separator + randString)
     .get('link[rel="canonical"]')
     .should('have.length', 1)
     .and('have.attr', 'href', expectedCanonical)
