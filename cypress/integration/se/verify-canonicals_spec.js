f4c6822ec44141c1c73275b1b403d7f3441d6db0
Joacim Boive <joacim.boive@gmail.com>
2017-10-27 13:57:02 +0200

    Fixed for Canonicals

--- cypress/integration/se/verify-canonicals_spec.js
+++ cypress/integration/se/verify-canonicals_spec.js
@@ -1,22 +1,35 @@
 import * as utils from '../../support/utils';
 
 const list = new Map();
-const domain = 'https://www.lensway.se';
+const ROOT_URL = 'https://sheets.googleapis.com/v4/spreadsheets';
+const SPREADSHEET_ID = '1OFArx_Ilp7QZxFnc23_lbMBYaihtY118n7qSZaJPfGo';
+const API_KEY = 'AIzaSyDIpJedu-YW3-EVYAwfA4DLpUdvnSBFhlE';
+const RANGE_CANONICALS = 'canonicals!A2:B500';
 
-list.set(`${domain}/glasogon/glasogonlista`, `${domain}/glasogon`);
-list.set(`${domain}/lenses`, `${domain}/kontaktlinser`);
-list.set(`${domain}/lenses?allList=true`, `${domain}/kontaktlinser`);
-list.set(`${domain}/kontaktlinser/linslista`, `${domain}/kontaktlinser`);
-list.set(`${domain}/glasogon/glasogonlista?clearFilters`, `${domain}/glasogon`);
-list.set(`${domain}/solglasogon?p_shape=Butterfly`, `${domain}/solglasogon`);
-list.set(
-  `${domain}/glasogon/glasogonlista/carrera?p_price=f496t995&sort=popularity&_page=1`,
-  `${domain}/glasogon/glasogonlista/carrera`
-);
+let sheetData = [];
+
+before(function() {
+  return fetch(`${ROOT_URL}/${SPREADSHEET_ID}/values/${RANGE_CANONICALS}?key=${API_KEY}`, {
+    method: 'GET',
+    mode: 'cors',
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('There was a problem with the request for Google Sheet data');
+    })
+    .then(data => {
+      sheetData = data.values;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+});
 
 describe('Verify Canonicals', () => {
   it('Should visit each URL in the list successfully and point to the correct canonical', () => {
-    list.forEach((shouldPointTo, url) => {
+    sheetData.forEach(([url, shouldPointTo]) => {
       utils.testCanonical(url, shouldPointTo, `cid=${new Date().getTime()}`);
     });
   });
