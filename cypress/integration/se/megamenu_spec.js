946572cb4630b8f6d3f1bf804562614c956f657d
Joacim Boive <joacim.boive@gmail.com>
2017-10-27 14:50:57 +0200

    Fix for megamenu

--- cypress/integration/se/megamenu_spec.js
+++ cypress/integration/se/megamenu_spec.js
@@ -1,13 +1,23 @@
 import * as utils from '../../support/utils';
-//test
+
+let sheetData = [];
+
+before(() => {
+  return utils.getSheetData('megamenu!A2:A500').then(data => {
+    return (sheetData = data);
+  });
+});
+
 describe('Megamenu top links', () => {
   it('Should have ONE canonical link that points to the correct URL', () => {
-    cy
-      .visit('https://www.lensway.se/')
-      .get('.header-large__nav-main a')
-      .each(link => {
-        utils.testCanonical(link[0].href, link[0].href);
-        utils.testCanonical(link[0].href, link[0].href, `cid=${new Date().getTime()}`);
-      });
+    sheetData.forEach(([url]) => {
+      cy
+        .visit(url)
+        .get('.header-large__nav-main a')
+        .each(link => {
+          utils.testCanonical(link[0].href, link[0].href);
+          utils.testCanonical(link[0].href, link[0].href, `cid=${new Date().getTime()}`);
+        });
+    });
   });
 });
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,3 +1,26 @@
+const ROOT_URL = 'https://sheets.googleapis.com/v4/spreadsheets';
+const SPREADSHEET_ID = '1OFArx_Ilp7QZxFnc23_lbMBYaihtY118n7qSZaJPfGo';
+const API_KEY = 'AIzaSyDIpJedu-YW3-EVYAwfA4DLpUdvnSBFhlE';
+
+export const getSheetData = (range) => {
+  return fetch(`${ROOT_URL}/${SPREADSHEET_ID}/values/${range}?key=${API_KEY}`, {
+    method: 'GET',
+    mode: 'cors',
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('There was a problem with the request for Google Sheet data');
+    })
+    .then(data => {
+      return data.values;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+};
+
 export const testCanonical = (url, expectedCanonical, randString = '') => {
   let separator = '';
 
