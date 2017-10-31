d38e8c6ddfa7eb70e74a9aa0543d0da5aa3d1748
Joacim Boive <joacim.boive@gmail.com>
2017-10-31 08:17:39 +0100

    Documented test.js

--- cypress/support/get.js
+++ cypress/support/get.js
@@ -2,9 +2,14 @@ import { URL, ID, KEY } from '../../sheetConfig';
 
 const get = {};
 
+/**
+ * Fetch data from a configured Google Sheet tab
+ * @param tabName {string} The name of the tab to get data from: canonicals, megamenu, title
+ * @param rows {number} The number of rows to return (Truncates to the last row containing data)
+ */
 const sheet = (tabName, rows) => {
   const COL_A = 'A2';
-  return fetch(`${URL}/${ID}/values/${tabName}!${COL_A}:B${rows}?key=${KEY}`, {
+  return fetch(`${URL}/${ID}/values/${tabName}!${COL_A}:B${rows.toString()}?key=${KEY}`, {
     method: 'GET',
     mode: 'cors',
   })
--- cypress/support/test.js
+++ cypress/support/test.js
@@ -1,5 +1,15 @@
 const test = {};
 
+/**
+ * Loads an URL twice - using a GET request and checks the canonical link against the expected result.
+ * Second time it appends a query string and validates the URL again against the canonical link.
+ * @param url {string} The url to test against
+ * @param expectedCanonicalUrl {string} The canonical should be this.
+ * Ex;
+ * Expecting: <link rel="canonical" href="https://www.lensway.se/kontaktlinser">
+ * You provide the href-attribute: https://www.lensway.se/kontaktlinser
+ * @param randString {string} A random string to append as a query parameter.
+ */
 const canonical = (url, expectedCanonicalUrl, randString = '') => {
   let separator = '';
 
